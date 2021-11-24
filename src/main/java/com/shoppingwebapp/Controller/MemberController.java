3abd62341f02093a349fea6a6488b427593305f8
Ghosts381937 <she.xing88@gmail.com>
2021-11-24 19:39:35 +0800

    Enable CORS in MemberController.java and ProductController.java

--- src/main/java/com/shoppingwebapp/Controller/MemberController.java
+++ src/main/java/com/shoppingwebapp/Controller/MemberController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @Controller // This means that this class is a Controller
 @RequestMapping(path = "/member") // This means URL's start with /demo (after Application path)
 public class MemberController {

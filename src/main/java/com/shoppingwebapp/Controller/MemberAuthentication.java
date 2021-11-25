612d37e1d94192fbca764cad29e5017dc9a54e61
Ghosts381937 <she.xing88@gmail.com>
2021-11-25 15:44:37 +0800

    write an example for spring session in MemberAuthentication.java

--- src/main/java/com/shoppingwebapp/Controller/MemberAuthentication.java
+++ src/main/java/com/shoppingwebapp/Controller/MemberAuthentication.java
@@ -1,4 +1,21 @@
 package com.shoppingwebapp.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
 public class MemberAuthentication {
+
+    @GetMapping("/set")
+    public String set(HttpSession session) {
+        session.setAttribute("user", "test");
+        return "success";
+    }
+
+    @GetMapping("/get")
+    public String get(HttpSession session) {
+        return "Session id:" + session.getAttribute("user");
+    }
 }

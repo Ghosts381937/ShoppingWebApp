aa0180e0909b45acba7c1b4a8c3452cada779b88
Ghosts381937 <she.xing88@gmail.com>
2021-11-25 15:49:37 +0800

    add some comment for spring session example

--- src/main/java/com/shoppingwebapp/Controller/MemberAuthentication.java
+++ src/main/java/com/shoppingwebapp/Controller/MemberAuthentication.java
@@ -8,13 +8,13 @@ import javax.servlet.http.HttpSession;
 @RestController
 public class MemberAuthentication {
 
-    @GetMapping("/set")
+    @GetMapping("/set")//set a session id for "test" then return to client
     public String set(HttpSession session) {
         session.setAttribute("user", "test");
         return "success";
     }
 
-    @GetMapping("/get")
+    @GetMapping("/get")//use the session id on the client to get the username
     public String get(HttpSession session) {
         return "Session id:" + session.getAttribute("user");
     }

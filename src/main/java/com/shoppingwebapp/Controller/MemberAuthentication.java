774d31fb667aaac5b75bd7609e9cd45103e3e4ab
tim970303 <tim970303@gmail.com>
2021-11-28 13:50:23 +0800

    login and add session

--- src/main/java/com/shoppingwebapp/Controller/MemberAuthentication.java
+++ src/main/java/com/shoppingwebapp/Controller/MemberAuthentication.java
@@ -1,12 +1,33 @@
 package com.shoppingwebapp.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.shoppingwebapp.Dao.MemberRepository;
+import com.shoppingwebapp.Model.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
-@RestController
+@RestController // This means that this class is a Controller
+@RequestMapping(path = "/auth") // This means URL's start with /demo (after Application path)
 public class MemberAuthentication {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @PostMapping(path = "/login") //Create member
+    public String login(@RequestParam String username, @RequestParam String password,HttpSession session) {
+        Iterable<Member> iterable = memberRepository.findByUsername(username);
+
+        for (Member member : iterable) {
+            if(member.getPassword().equals(password)){
+                if(session.getAttribute(username) == null){
+                    session.setAttribute(username, member.getId());
+                }
+                return "Success";
+            }
+        }
+        return "Fail";
+    }
+
 
     @GetMapping("/set")//set a session id for "test" then return to client
     public String set(HttpSession session) {
